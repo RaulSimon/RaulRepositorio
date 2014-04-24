4ef35e35ebf8d0d044286fc7a77c5df51fd73295
RaulSimon <raulcapistrano55@gmail.com>
2014-04-24 10:08:40 -0300

    Exercicios

---
+++ exe10/exe10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               String[] NomeProduto = new String[4];
+            double[] preco = new double[4];
+            double[]  precoDesconto = new double[4];
+            double a=0,b;
+            for (int i = 0; i < 4; i ++ )
+            {
+                NomeProduto[i] = Console.ReadLine();
+                preco[i] = double.Parse(Console.ReadLine());
+                precoDesconto[i] = double.Parse(Console.ReadLine());
+                Console.WriteLine("Nome Produto: " + NomeProduto[i] + " Preço produto " + preco[i] + "  Desconto " + precoDesconto[i]);
+                b = preco[i] * precoDesconto[i];
+                preco[i] -= b;
+                Console.WriteLine("  produto Com desconto  : " + preco[i]);
+                a += preco[i];
+            }
+            Console.WriteLine("preço total: "+ a);
+            Console.ReadKey();
+        }
+        }
+    }
+}
---
+++ exe9/exe9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = 12;
+
+            if (a + b > c)
+            {
+                Console.WriteLine("maior");
+            }
+            else if (a + b < c)
+            {
+                Console.WriteLine("menor");
+            }
+            else
+            {
+                Console.WriteLine("igual");
+            }
+            Console.ReadKey();
+        }
+    }
+}
