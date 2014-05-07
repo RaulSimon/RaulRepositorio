eecec7b0277be68e4775f74988b285e9531a0d8f
RaulSimon <raulcapistrano55@gmail.com>
2014-05-07 10:21:51 -0300

    c

---
+++ RaulRepositorio/Unidade7/Operadores/Atribuicao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade7.Operadores
+{
+    class Atribuicao
+    {
+        static void Main(String[] args){
+            double uan = 1;
+            uan += 2;
+            uan -= 4;
+            uan *= 25;
+            uan /= 20;
+            
+            Random r = new Random();
+            uan = r.NextDouble() * 100;
+           if(uan > 10 && uan < 100){
+               Console.WriteLine("lol");
+           }else if(uan > 10 || uan <100){
+               Console.WriteLine("sdahudusa");
+           }
+            Console.WriteLine("Valor do uan: " + uan);
+            Console.ReadKey();
+        }
+    }
+}
