64a345ff4e3836b684bcf152d54d39c56ab3bb7b
RaulSimon <raulcapistrano55@gmail.com>
2014-05-14 11:12:53 -0300

    novos fors

--- RaulRepositorio/Unidade8/Repeticao/Repeticao.cs
+++ RaulRepositorio/Unidade8/Repeticao/Repeticao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +11,21 @@ namespace Unidade8.Repeticao
     {
         static void Main(String[] args)
         {
-           //  imprime100numeros();
+            //  imprime100numeros();
             //imprime100MenosMutiplosDe3();
-            MaiorNumeroInteiro();
+            //MaiorNumeroInteiro();
+            //JogoDeDados2();
+            Castigo(20);
             Console.ReadKey();
         }
 
         public static void imprime100numeros()
         {
-            
+
 
             for (int i = 0; i < 100; i++)
             {
-                
+
                 Console.WriteLine(i);
             }
         }
@@ -31,10 +34,10 @@ namespace Unidade8.Repeticao
         {
             for (int i = 0; i < 100; i++)
             {
-                if (i%3 == 0)
+                if (i % 3 == 0)
                 {
                     continue;
-                    
+
                 }
                 else
                 {
@@ -49,10 +52,61 @@ namespace Unidade8.Repeticao
             Console.WriteLine(maior);
             while (maior > 100)
             {
-                maior = maior/2;
+                maior = maior / 2;
                 Console.WriteLine(maior);
 
             }
+        }
+
+        public static void JogoDeDados()
+        {
+            Random r = new Random();
+            int dado, soma = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                dado = r.Next(1, 7);
+                soma += dado;
+                if (soma > 19)
+                {
+                    Console.WriteLine("Maior que 19" + soma);
+                    break;
+
+                }
+            }
+            Console.WriteLine("Final das 5 jogadas: " + soma);
+        }
+
+        public static void JogoDeDados2()
+        {
+            Random r = new Random();
+            int dado, soma = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                dado = r.Next(1, 7);
+                if (dado % 2 == 1)
+                {
+
+
+                    soma += dado;
+                }
+
+                if (soma > 9)
+                {
+                    Console.WriteLine("Maior que 9 : " + soma);
+                    break;
+
+                }
+            }
+            Console.WriteLine("Final das 4 jogadas: " + soma);
+        }
+
+        public static void Castigo(int quantidadeDeCastigo)
+        {
+            for (int i = 0; i < quantidadeDeCastigo; i++)
+            {
+            Console.WriteLine( "Eu nao vou mais andar de skate nos corredores ");    
+            }
+            
         }
     }
 }
