bbc38a9f180133524fad6010f38d6221844917d8
RaulSimon <raulcapistrano55@gmail.com>
2014-06-09 10:14:49 -0300

    Banco de dados

---
+++ RaulRepositorio/Unidade11/ExercicioComplementar/megaSena.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade11.ExercicioComplementar
+{
+    class megaSena
+    {
+        static void Main()
+        {
+            sorteioMegaSena();
+            Console.ReadKey();
+
+
+        }
+
+        static void sorteioMegaSena()
+        {
+            Random r = new Random();
+            int[] sorteioDosNumeros = new int[6];
+            int aux = 0;
+            for (int i = 0; i < sorteioDosNumeros.Length; i++)
+            {
+                sorteioDosNumeros[i] = r.Next(1,61);
+                
+                aux = NumeroEscolhidos(sorteioDosNumeros);
+                Console.WriteLine("Numero {0} sorteado",sorteioDosNumeros[i]);
+            }
+            if (aux == 1 )
+            {
+                Console.WriteLine("Acertou 1");
+            }
+            else if (aux ==2)
+            {
+                Console.WriteLine("Acertou 2");
+            }
+            else if (aux ==3)
+            {
+                Console.WriteLine("Acertou 3");
+            }
+            else if (aux ==4)
+            {
+                Console.WriteLine("Acertou 4");
+            }
+            else if (aux ==5)
+            {
+                Console.WriteLine("Acertou 5");
+            }
+            else if (aux ==6)
+            {
+                Console.WriteLine("Acertou 6");
+            }
+            else
+            {
+                Console.WriteLine("Nao acertou");
+            }
+            for (int i = 0; i < sorteioDosNumeros.Length; i++)
+            {
+                OrdenaElemento(sorteioDosNumeros);
+                Console.WriteLine(sorteioDosNumeros[i]);
+            }
+            
+
+        }
+
+        static int  NumeroEscolhidos(int[] sorteioDosNumeros)
+        {
+            int[] informarNumeros = new int[6];
+            Random r= new Random();
+            int contador = 0;
+            for (int i = 0; i < informarNumeros.Length; i++)
+            {
+                informarNumeros[i] = r.Next(1, 61);
+                sorteioDosNumeros.Contains(informarNumeros[i]);
+
+            }
+            return contador;
+        }
+        private static int menor(int[] sorteioDosNumeros)
+        {
+            int posicaoMenor = 0;
+            int auxMenor = 0;
+            for (int i = 1; i < sorteioDosNumeros.Length; i++)
+            {
+
+
+                if (sorteioDosNumeros[posicaoMenor] > sorteioDosNumeros[i])
+                {
+                    sorteioDosNumeros[posicaoMenor] = i;
+                    auxMenor = sorteioDosNumeros[i];
+                }
+            }
+            return auxMenor;
+        }
+
+        
+        private static void troca(int[] sorteioDosNumeros, int i, int j)
+        {
+            int aux = sorteioDosNumeros[i];
+            sorteioDosNumeros[i] = sorteioDosNumeros[j];
+            sorteioDosNumeros[j] = aux;
+        }
+        public static int Menor(int[] sorteioDosNumeros, int inicio)
+        {
+            int menor = inicio;
+
+            for (int i = inicio + 1; i < sorteioDosNumeros.Length; i++)
+            {
+                if (sorteioDosNumeros[menor] > sorteioDosNumeros[i])
+                {
+                    menor = i;
+                }
+            }
+            return menor;
+        }
+        private static void OrdenaElemento(int[] sorteioDosNumeros)
+        {
+
+            for (int i = 0; i < sorteioDosNumeros.Length; i++)
+            {
+                int menor = Menor(sorteioDosNumeros, i);
+                troca(sorteioDosNumeros, i, menor);
+
+            }
+
+        }
+    }
+}
---
+++ RaulRepositorio/Unidade11/ExercicioFixacao/fixacao.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade11.ExercicioFixacao
+{
+    class fixacao
+    {
+        
+
+        static void Main1()
+        {
+            int[] array = {1,4,6,3,74,3,53,73,35,3};
+            
+              int ElementoMaior = maior(array);
+            Console.WriteLine("Maior elemente {0}  ",ElementoMaior);
+            int ElementoMenor = menor(array);
+            Console.WriteLine("Elemente menor {0} ", ElementoMenor);
+            int SomaElementos = soma(array);
+            Console.WriteLine("Soma dos elementos {0}",SomaElementos);
+            int mediaElementos = media(array);
+            Console.WriteLine("Media dos Elementos {0} ",mediaElementos);
+            
+            
+            
+            OrdenaElemento(array);
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);    
+            }
+            
+            Console.ReadKey();
+        }
+
+        
+
+        private static int media(int[] array)
+        {
+            int media;
+            int Soma = soma(array);
+            media = Soma/array.Length;
+            return media;
+        }
+
+        private static int soma(int[] array)
+        {
+            int soma = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                soma += array[i];
+            }
+            return soma;
+        }
+
+        private static int maior(int[] array)
+        {
+            int posicaoMaior = 0;
+            int auxMaior=0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[posicaoMaior] < array[i])
+                {
+                    array[posicaoMaior] = i;
+                    auxMaior = array[i];
+
+                }
+            }
+            return auxMaior;
+        }
+
+        private static int menor(int[] array)
+        {
+            int posicaoMenor = 0;
+            int auxMenor = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+
+
+                if (array[posicaoMenor] > array[i])
+                {
+                    array[posicaoMenor] = i;
+                    auxMenor = array[i];
+                }
+            }
+            return auxMenor;
+        }
+
+        private static void troca(int[] array, int i, int j)
+        {
+            int aux = array[i];
+            array[i] = array[j];
+            array[j] = aux;
+        }
+        public static int Menor(int[] array, int inicio)
+        {
+            int menor = inicio;
+
+            for (int i = inicio + 1; i < array.Length; i++)
+            {
+                if (array[menor] > array[i])
+                {
+                    menor = i;
+                }
+            }
+            return menor;
+        }
+        private static void OrdenaElemento(int[] array)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int menor = Menor(array, i);
+                troca(array,i,menor);
+                
+            }
+            
+        }
+    }
+}
